9e7c91049cff132173bfd37a1eab000f0b8e267d
Chris Sanders <sanders.chris@gmail.com>
2017-03-31 18:34:36 -0500

    cleaned up

--- provides.py
+++ provides.py
@@ -9,9 +9,6 @@ class SaltProvides(RelationBase):
 
     @hook('{provides:salt}-relation-{joined,changed}')
     def changed(self):
-        print("Provides realation joined/changed called")
-        localMinion = self.get_local('minion')
-        remoteMinion = self.get_remote('minion')
         if self.get_local('address') is None:
             self.set_state('{relation_name}.unconfigured')
         if self.get_remote('minion'):
@@ -24,13 +21,11 @@ class SaltProvides(RelationBase):
     @property
     def minion(self):
         return self.get_remote('minion')
-
     def configure(self,address,port):
         relation_info = {
             'address': address,
             'port': port
              }
-        print("SaltProvides configured")
         self.set_remote(**relation_info)
         self.set_local(**relation_info)
         self.remove_state('{relation_name}.unconfigured')
--- requires.py
+++ requires.py
@@ -10,24 +10,17 @@ class SaltRequires(RelationBase):
 
     @hook('{requires:salt}-relation-{joined,changed}')
     def changed(self):
-        conv = self.conversation()
-        print("Requires relation joined/changed called")
-        if conv.get_remote('address'):
-            conv.set_state('{relation_name}.changed')
+        if self.get_remote('address'):
+            self.set_state('{relation_name}.changed')
 
     @hook('{requires:salt}-relation-{departed}')
     def departed(self):
-        conv = self.conversation()
-        conv.remove_state('{relation_name}.available')
+        self.remove_state('{relation_name}.available')
 
     @property
     def address(self):
         return self.get_remote('address')
-        #conv = self.conversation()
-        #return conv.get_remote('hostname')
 
     def minion_ready(self):
-        #conv = self.conversation()
-        print("salt-minion is ready")
         self.set_remote("minion",socket.getfqdn())
 
